9b9e856e3054e27cdfd416f2a61c31601d3bc0ed
DanielAlfonso17 <daniel.alfonso-b@mail.escuelaing.edu.co>
2020-07-06 18:25:45 -0500

    Push heroku

--- src/main/java/edu/escuelaing/arsw/tictactoe/App.java
+++ src/main/java/edu/escuelaing/arsw/tictactoe/App.java
@@ -6,6 +6,9 @@ import java.util.Map;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.style.ToStringCreator;
+
+
 
 
 
@@ -13,22 +16,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class App 
 {
 	public static void main(String[] args){
-        SpringApplication.run(App.class, args);
-        SpringApplication application = new SpringApplication(App.class);
-        
-        Map<String, Object> pro = new HashMap<String, Object>();
-        pro.put("server port", getPort());
-        application.setDefaultProperties(pro);
-        application.run(args);
-      
-        
-
-    }
-	static int getPort() {
-        if (System.getenv("PORT") != null) {
-            return Integer.parseInt(System.getenv("PORT"));
-        }
-        return 8080; //returns default port if heroku-port isn't set (i.e. on localhost)
-    }
-	
+		 
+			SpringApplication app = new SpringApplication(App.class);
+			String puerto = getPort()+"";
+			System.setProperty("server.port", puerto);
+	        app.run(args);
+	    }
+	    
+	    static int getPort() {
+	        if (System.getenv("PORT") != null) {
+	            return Integer.parseInt(System.getenv("PORT"));
+	        }
+	        return 50000; //returns default port if heroku-port isn't set (i.e. on localhost)
+	    }
 }
